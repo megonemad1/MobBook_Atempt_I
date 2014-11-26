a31a97868820ca88d8e7ea052e61a5bcd4e3eb11
megonemad1 <rhys.puddephatt@btinternet.com>
2014-11-26 17:03:52 +0000

    Html Stuff

--- MobBook/MobBook_Main_Views.py
+++ MobBook/MobBook_Main_Views.py
@@ -3,8 +3,8 @@ from flask import Flask,render_template,request
 from MobBook import app
 
 
-@app.route('/',methods=['GET','POST'])
-@app.route('/<username>',methods=['GET','POST'])
+
+@app.route('/ID/<username>',methods=['GET','POST'])
 def Param(username="world"):
 	optid=username
 	if (request.method=='POST'):
@@ -12,3 +12,10 @@ def Param(username="world"):
 		print(request.form['options'])
 	return render_template('Basic_Layout.html',	option_list=[{'optid':"Dynamic1"},{'optid':"Dynamic2"},{'optid':"spotify"},{'optid':"cheese"}], optid=optid)
 
+@app.route('/',methods=['GET','POST'])
+def home():	
+	optid=""
+	if (request.method=='POST'):
+		optid= (request.form['options'])
+		print(request.form['options'])
+	return render_template('Basic_Layout.html',	option_list=[{'optid':"Dynamic1"},{'optid':"Dynamic2"},{'optid':"spotify"},{'optid':"cheese"}], optid=optid)
